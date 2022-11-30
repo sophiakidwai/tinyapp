9b3f77f496f8abd1a4635cc85d943a7e01eccf28
sophia kidawai <sophia.kidwai@telus.com>
2022-11-30 14:35:41 -0800

    fixed urls id route

--- express_server.js
+++ express_server.js
@@ -26,22 +26,37 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  // console.log(urlDatabase) 
+  Object.assign(urlDatabase, req.body);
+  console.log(urlDatabase)
+  res.redirect("/urls/:id");
+  // res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/u/:id", (req, res) => {   
+  const longURL = urlDatabase.longURL;
+    res.redirect(longURL);
+  });
+
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: "http://www.lighthouselabs.ca" };
+// browser is requesting this URL http://localhost:8080/urls/9sm5xK
+
+app.get("/urls/:myid", (req, res) => {
+  const templateVars = { 
+    id: req.params.myid, 
+    longURL: urlDatabase[req.params.myid]
+  };
   res.render("urls_show", templateVars);
 });
 
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
